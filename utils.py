d8462e44e241af3f60a04626b3896cdbc5d35475
Elliot Layne <e_layne@sympatico.ca>
2019-12-01 17:51:28 -0500

    adding dataset split to utils

--- utils.py
+++ utils.py
@@ -43,4 +43,33 @@ def train_valid_split_indices(max_index, validation_percentage=0.3, min_index=0,
     return train_indices, valid_indices
 
 
-recovered = load_json('/home/elliot/PycharmProjects/abide/processed_data_files/asd_raw/raw_img_asd.json')
+def load_dataset(asd_path, ctl_path, seed=7):
+    x_train = list()
+    x_valid = list()
+    y_train = list()
+    y_valid = list()
+
+    asd_data = load_json(asd_path)
+    asd_keys = list(asd_data.keys())
+    train_indices, valid_indices = train_valid_split_indices(max_index=len(asd_keys), random_seed=seed)
+    for idx in train_indices:
+        x_train.append(asd_data[asd_keys[idx]])
+        y_train.append(1.0)  # all positive cases
+    for idx in valid_indices:
+        x_valid.append(asd_data[asd_keys[idx]])
+        y_valid.append(1.0)  # all positive cases
+    asd_data.clear()
+
+    # now the negative case loop
+    ctl_data = load_json(ctl_path)
+    ctl_keys = list(ctl_data.keys())
+    for idx in train_indices:
+        x_train.append(ctl_data[ctl_keys[idx]])
+        y_train.append(0.0)  # all negative cases
+    for idx in valid_indices:
+        x_valid.append(ctl_data[ctl_keys[idx]])
+        y_valid.append(0.0)  # all negative cases
+    ctl_data.clear()
+
+    return np.asarray(x_train), np.asarray(y_train), np.asarray(x_valid), np.asarray(y_valid)
+# recovered = load_json('/home/elliot/PycharmProjects/abide/processed_data_files/asd_raw/raw_img_asd.json')
